6ea1bda16c1f6818f2e682e8d461d479d0002df2
Poovarasan S <112718552+PoovarasanSundaramoorthi@users.noreply.github.com>
2024-03-06 21:52:54 +0530

    soruce file upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Routers from './router/inde';
+
+function App() {
+  return (
+    <Routers />
+  );
+}
+
+export default App;
---
+++ src/router/inde.js
@@ -0,0 +1,21 @@
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+// import Spinner from "react-bootstrap/Spinner";
+const Dashboard = lazy(() => import('../pages/UserDashboard'));
+const LoginPage = lazy(() => import('../pages/Login'));
+export default function Routers() {
+
+    return (
+        <>
+            <Router>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/login" replace />} />
+                        <Route path="/home-page/:userId" element={<Dashboard />} />
+                        <Route path="/login" element={<LoginPage />} />
+                    </Routes>
+                </Suspense>
+            </Router>
+        </>
+    );
+}
